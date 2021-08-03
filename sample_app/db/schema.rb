a9b6da3af9307768fc752bb9afbd8115621be3e5
EC2 Default User <ec2-user@ip-172-31-19-55.ec2.internal>
2021-08-03 09:36:43 +0000

    [Update]データ破損のため更新

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_17_024025) do
+ActiveRecord::Schema.define(version: 2021_08_03_065353) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
