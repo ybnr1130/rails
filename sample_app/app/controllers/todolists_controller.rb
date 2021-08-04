d22e2cdf6de40f636cded936ee42cce0e68166fb
EC2 Default User <[muuckk@gmail.com]>
2021-08-04 04:46:30 +0000

    [Finish]2-8

--- sample_app/app/controllers/todolists_controller.rb
+++ sample_app/app/controllers/todolists_controller.rb
@@ -3,9 +3,12 @@ class TodolistsController < ApplicationController
     @list = List.new
   end
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to todolist_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to todolist_path(list.id)
+    else
+      render :new
+    end    
   end
   def index
     @lists = List.all
@@ -28,3 +31,4 @@ class TodolistsController < ApplicationController
   end
     
 end
+
